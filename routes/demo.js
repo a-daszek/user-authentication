9b6082f6252c1a11defa5c420196cfbed79ee260
Anna Daszyńska <anna.daszynska@protonmail.com>
2023-06-25 15:57:37 +0200

    first commit

---
+++ routes/demo.js
@@ -0,0 +1,81 @@
+const express = require('express');
+
+const bcrypt = require("bcryptjs");
+
+const db = require('../data/database');
+
+const router = express.Router();
+
+router.get('/', function (req, res) {
+  res.render('welcome');
+});
+
+router.get('/signup', function (req, res) {
+  res.render('signup');
+});
+
+router.get('/login', function (req, res) {
+  res.render('login');
+});
+
+router.post('/signup', async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"]; //in square bracket because of the dash
+  const enteredPassword = userData.password;
+
+  if (!enteredEmail || !enteredConfirmEmail || !enteredPassword || enteredPassword.trim() < 6 || enteredEmail !== enteredConfirmEmail || !enteredEmail.includes("@")){
+    console.log("Bad data!");
+    return res.redirect("/signup");
+  }
+
+  const existingUser = await db.getDb().collection("users").findOne({email:enteredEmail});
+
+  if (existingUser){
+    console.log("User already exists.")
+    return res.redirect("/signup");
+  }
+
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
+
+  const user = {
+    email: enteredEmail,
+    password: hashedPassword
+  }
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
+});
+
+router.post('/login', async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+
+  const existingUser = await db.getDb().collection("users").findOne({email:enteredEmail});
+
+  if (!existingUser){
+    console.log("Couldn't log in.");
+    return res.redirect("/login");
+  }
+
+  const passwordsAreEqual = await bcrypt.compare(enteredPassword, existingUser.password);
+
+  if(!passwordsAreEqual){
+    console.log("Couldn't log in - passwords are not matching.");
+    return res.redirect("/login");
+  }
+
+  console.log("User is authenticated.");
+  res.redirect("/admin");
+
+});
+
+router.get('/admin', function (req, res) {
+  res.render('admin');
+});
+
+router.post('/logout', function (req, res) {});
+
+module.exports = router;
